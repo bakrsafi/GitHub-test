f2bea86761fa0f8baafeea6f9d55c314c4c9fe5d
bakr-safi <bakrsafi.escanor@gmail.com>
2025-10-27 00:50:43 +0300

    last

--- test.py
+++ test.py
@@ -1,17 +1,6 @@
-testv1
-
-
-
-sortedf
-asdf
-asdf
-sadf
-
-asdf
-a
-sdf
-sorteddf
-sdf
-sorteddf
-sdf
-sdf
+def test_addition():
+    assert 2 + 2 == 4      
+def test_subtraction():
+    assert 5 - 3 == 2
+def test_multiplication():
+    assert 3 * 4 == 12
